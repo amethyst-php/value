5eab04b520bef2a1aa8c58f209e11b1fe8f79c64
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ValueManager.php
+++ src/Managers/ValueManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Value newEntity()
+ * @method \Amethyst\Schemas\ValueSchema getSchema()
+ * @method \Amethyst\Repositories\ValueRepository getRepository()
+ * @method \Amethyst\Serializers\ValueSerializer getSerializer()
+ * @method \Amethyst\Validators\ValueValidator getValidator()
+ * @method \Amethyst\Authorizers\ValueAuthorizer getAuthorizer()
+ */
 class ValueManager extends Manager
 {
     use ConfigurableManager;
